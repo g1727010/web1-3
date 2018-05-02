818e5fcf97f93f635299af6314df3ae717b55f87
Unknown <katsuma4869@icloud.com>
2018-05-02 18:08:18 +0900

    最初のコミット

---
+++ Sample/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++)
+        {
+            sum += 1;
+        }
+
+        Debug.Log(sum);
+    }
+}
